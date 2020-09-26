12b793c95ec10ed7583108e6507a2365f6de060c
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-26 13:45:12 +0300

    Fix a docs-related Qemu problem with this patch: https://www.mail-archive.com/qemu-devel@nongnu.org/msg723079.html
    Signed-off-by: Artyom Gevorgyan <artemhevorhian@gmail.com>

--- build.sh
+++ build.sh
@@ -3,9 +3,14 @@
 git clone https://github.com/qemu/qemu && cd qemu
 git checkout v5.1.0
 
+# Apply a patch as a workaround to fix Qemu Sphinx-related problem
+git apply ../fix_qemu_sphinx_warning.patch
+
 # Now tweak the build options for Linux target OS. We maintain a patch
 # for that purpose.
 git apply ../tweak_linux_config_options.patch
+
+echo 'Successfully applied the required patches. Building now...'
 
 mkdir build-riscv64 && cd build-riscv64
 ../configure --target-list=riscv64-softmmu --static --audio-drv-list=""  --without-default-devices --disable-vnc
