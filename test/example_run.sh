f2f0c276c399916ac66c5dab99d58d3ca48e6d3e
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-30 13:00:14 +0300

    Success. Format notes.md as a report of what has been done.
    Signed-off-by: Artyom Gevorgyan <artemhevorhian@gmail.com>

--- test/example_run.sh
+++ test/example_run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-IMAGE=./softmmu/hifive ./qemu-system-riscv64 -m 1g -machine sifive_u,start-in-flash=true -nographic -device loader,addr=0x20000000,file=${IMAGE} -bios none -smp 4
+./qemu-system-riscv64 -m 1g -machine sifive_u,start-in-flash=true -nographic -device loader,addr=0x20000000,file=softmmu/hifive -bios none -smp 4
